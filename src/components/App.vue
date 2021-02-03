ddd1fa9ea7c29f633f54f8d980cff2ccbcb7be5f
Robert Timothy Yap <yap.rtim@gmail.com>
2021-02-03 23:39:37 +0800

    Add sample app

---
+++ src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data() {
+        return {
+            greeting: 'Greetings from Vue 2'
+        };
+    }
+};
+</script>
