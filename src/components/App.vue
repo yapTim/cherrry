bbcdd9b1a24026eeae627542962cc6d65729623d
Robert Timothy Yap <yap.rtim@gmail.com>
2021-02-07 14:24:45 +0800

    Add products page in root

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="container is-max-widescreen">
-        <div></div>
         <Navbar />
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 import Navbar from './Navbar.vue';
+import Products from './Products.vue';
 
 export default {
     name: 'App',
 
     components: {
-        Navbar
+        Navbar,
+        Products
     },
 
     data() {
         return {
             greeting: 'Greetings from Vue 2'
         };
+    },
+
+    created() {
+        this.$router.replace({
+            name: 'products-page'
+        })
     }
 };
 </script>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
-import App from './components/App.vue'
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: App }
+    { path: '/', component: () => import('./components/App.vue') },
+    { path: '/', component: () => import('./components/Products.vue'), name: 'products-page' },
 ];
 
 const router = new VueRouter({ routes, mode: 'history' });
