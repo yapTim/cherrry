03684b49622398bcb0f5928e9166df971807afdb
Robert Timothy Yap <yap.rtim@gmail.com>
2021-02-07 11:12:43 +0800

    Add ProductCard

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="product-card card mt-4">
+        <div class="card-image">
+            <figure class="image is-4by3">
+                <img src="src\assets\pictures\bracelet.jpg" alt="product-image" />
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="content">
+                This is a product {{ idx }}
+            </div>
+        </div>
+        <footer class="card-footer">
+            <div class="card-footer-item">Buy Now</div>
+            <div class="card-footer-item">Add to Card</div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+
+    props: {
+        idx: {
+            type: Number,
+        }
+    }
+}
+</script>
