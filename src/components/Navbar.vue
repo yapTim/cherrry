f160e285b48c88f68020d5f1581630552551d9d8
Robert Timothy Yap <yap.rtim@gmail.com>
2021-02-07 22:47:18 +0800

    Add Cart sidebar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,51 @@
 <template>
-    <b-navbar>
-        <template #brand>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template #end>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Cart</strong>
-                    </a>
-                </div>
-            </b-navbar-item>
-        </template>
-    </b-navbar>
+    <div>
+        <b-navbar>
+            <template #brand>
+                <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                    <img
+                        src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                        alt="Lightweight UI components for Vue.js based on Bulma"
+                    >
+                </b-navbar-item>
+            </template>
+            <template #end>
+                <b-navbar-item tag="div">
+                    <div class="buttons">
+                        <button class="button is-primary" @click="openSidebar = true">
+                            <strong>Cart</strong>
+                        </button>
+                    </div>
+                </b-navbar-item>
+            </template>
+        </b-navbar>
+        <Cart
+            :open-sidebar="openSidebar"
+            @cancel="cancelHandler"
+        />
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'Navbar',
+import Cart from './Cart.vue';
+
+export default {
+    name: 'Navbar',
+
+    components: {
+        Cart
+    },
+
+    data() {
+        return {
+            openSidebar: false
+        };
+    },
+
+    methods: {
+        cancelHandler() {
+            this.openSidebar = false;
+        }
     }
+}
 </script>
